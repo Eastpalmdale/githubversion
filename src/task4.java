c1af60d83fc92ffac8456cb838de3a32138691d0
Eastpalmdale <greer.kist@talawanda.org>
2024-02-06 14:06:07 -0500

    1:31pm

---
+++ src/task4.java
@@ -0,0 +1,9 @@
+public class task4 {
+    public static void main(String[] args) {
+        double yearlyBalance=1000;
+        double month=0;
+        boolean end=false;
+            for (int year = 0; year < 3; year++) {
+                yearlyBalance= yearlyBalance * 1.05;
+        System.out.println("Your balance after this year is " + yearlyBalance);
+    }}}
---
+++ src/task5.java
@@ -0,0 +1,14 @@
+public class task5 {
+    public static void main(String[] args) {
+        System.out.println("G****\nGR***\nGRE**\nGREE*\nGREER");
+
+
+
+
+
+
+
+
+
+
+    }}
