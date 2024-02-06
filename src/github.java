87f2dc1a61532dbd242ef36906155fa5f55b5b04
Eastpalmdale <greer.kist@talawanda.org>
2024-02-06 13:32:54 -0500

    1:31pm

---
+++ src/github.java
@@ -0,0 +1,12 @@
+public class github {
+    public static void main(String[] args) {
+        int sum=0;
+                sum=1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10;
+
+                System.out.println(sum);
+
+
+
+}
+
+}
